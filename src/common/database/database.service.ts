6a8c4992e4a7b2bf17c863438b74911cabc704eb
dinasalsabilla <dinasalsabilla05@gmail.com>
2026-04-26 17:33:27 +0700

    Fix Prisma client path

--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -1,26 +1,24 @@
 import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-// import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaClient } from './generated/prisma/client.js';
 import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class DatabaseService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
-  constructor(private configService: ConfigService) {
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
     const adapter = new PrismaPg({
       database: configService.get<string>('POSTGRE_DB'),
       host: configService.get<string>('POSTGRE_HOST'),
-      port: configService.get<number>('POSTGRE_PORT'),
+      port: Number(configService.get<number>('POSTGRE_PORT')),
       user: configService.get<string>('POSTGRE_USER'),
       password: configService.get<string>('POSTGRE_PASSWORD'),
     });
-    // const adapter = new PrismaNeon({
-    //   application_name: 'pharma-ease',
-    //   connectionString: env('DATABASE_URL_NEON'),
-    // });
-    super({ adapter: adapter });
+
+    super({ adapter });
   }
 
   async onModuleInit() {
